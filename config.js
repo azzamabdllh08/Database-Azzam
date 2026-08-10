386ddccebc1640c076bbf1cf61206c05cf1fd80b
azzamabdllh08 <azzamabdllh08@gmail.com>
2026-08-10 15:07:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
   brand: "Asset & Project",
   title: "SITE VERIFICATION DASHBOARD",
-  onedriveUrl: "https://1drv.ms/f/c/8dd2c5d0a467a39a/IgClQm7larpfSrYb9XAppmjdAeWrJr2SSBR5mqEHbgOrSFk?e=fBhTIu"
+  onedriveUrl: "https://1drv.ms/f/c/8dd2c5d0a467a39a/IgBGq-_P3IkZTYk2rsWBkd-VAb1oNTqVs2J6KM667FCvxqc?e=GYRHCX"
 };
