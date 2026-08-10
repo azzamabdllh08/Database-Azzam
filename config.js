b191d2ce0de5490986f48343691a0dabc2ca65ff
azzamabdllh08 <azzamabdllh08@gmail.com>
2026-08-10 14:52:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
   brand: "Asset & Project",
   title: "SITE VERIFICATION DASHBOARD",
-  source: "https://1drv.ms/f/c/8dd2c5d0a467a39a/IgClQm7larpfSrYb9XAppmjdAeWrJr2SSBR5mqEHbgOrSFk?e=fBhTIu"
+  onedriveUrl: "https://1drv.ms/f/c/8dd2c5d0a467a39a/IgClQm7larpfSrYb9XAppmjdAeWrJr2SSBR5mqEHbgOrSFk?e=fBhTIu"
 };
